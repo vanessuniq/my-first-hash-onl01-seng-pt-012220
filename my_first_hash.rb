cd206b98fb00983121e2d92f607d1a6fbcc1368c
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-02 01:09:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+  the_manifest = {}
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
